64422b700d51bc8725dd9d67d0fd6bba615bf39d
DARSHAN005 <96136603+DARSHAN005@users.noreply.github.com>
2022-02-01 09:03:41 +0530

    Add files via upload

---
+++ Java script practice/variable.js
@@ -0,0 +1,30 @@
+var x=10;
+let q=10;
+var x=6,y=8,z=10;
+let yz=9,xz=6,zz=3;
+var d=0.999;
+let pi=3.142456;
+console.log(x);
+console.log(y);
+console.log(d);
+console.log(pi);
+console.log(q);
+var variables=12345;
+let string="Hello WOrld";
+var qwerty="qwerty";
+const va=10;
+console.log(va);
+var BigInt=12355666655455520n;//BigInt
+let value=Symbol('hello')//symbol
+let val=Symbol('hello') //symbol
+let student={
+    id:101,
+    firstname:"Sachin",
+    lastname:"A",
+   class:10
+}; //object//Note ; except object all thr daatypes are primitive
+ console.log(student)
+ console.log(typeof(student))
+
+
+ 
